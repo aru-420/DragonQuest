5103f996d215478752833a0602705b97cf369df3
kandaKONA <ivynk.kanda@outlook.jp>
2022-06-09 15:30:34 +0900

    バトル画面HPバーの処理

--- app/src/main/java/com/example/dragonquest/BattleActivity.java
+++ app/src/main/java/com/example/dragonquest/BattleActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
 import com.example.dragonquest.databinding.ActivityBattleBinding;
 
 public class BattleActivity extends AppCompatActivity {
     private ActivityBattleBinding binding;
+
+    private Handler handler = new Handler();//スレッド内でテキストをいじる
+    private int myhp = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +23,17 @@ public class BattleActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        //メッセージ非表示
-        binding.battleMessage.setVisibility(View.INVISIBLE);
-        //メッセージのクリック処理有効化
-        binding.battleMessage.setClickable(true);
+        //初期非表示
+        binding.battleMessage.setVisibility(View.INVISIBLE);    //バトルメッセージ非表示
+        binding.battleEndButton.setVisibility(View.INVISIBLE);  //バトル終了ボタン非表示
+
+        //ライフゲージ周りの設定
+        binding.myHpBar.setMax(myhp);
+        binding.myHpBar.setProgress(myhp);
+        binding.ememyHpBar.setMax(myhp);
+        binding.ememyHpBar.setProgress(myhp);
+        binding.myHpText.setText(myhp + "/" + myhp);
+
 
         //左上ボタンクリック時の処理
         binding.skill1.setOnClickListener(new View.OnClickListener() {
@@ -31,15 +44,59 @@ public class BattleActivity extends AppCompatActivity {
             }
         });
 
+        //メッセージのクリック処理有効化
+        binding.battleMessage.setClickable(true);
+        //メッセージクリック時の処理
         binding.battleMessage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //メッセージの非表示
                 binding.battleMessage.setVisibility(View.INVISIBLE);
             }
-
         });
+
+        //HPが減る処理
+        int val = 1;
+        int maxHp = binding.myHpBar.getMax();   //hp最大値
+        //スレッド
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //HPが0になるまで
+                while (binding.myHpBar.getProgress() >= 0){
+                    //HPを１ずつ減らす
+                    myhp -= val;
+                    binding.myHpBar.setProgress(myhp);  //バーの表示更新
+                    handler.post(()->{
+                       binding.myHpText.setText(myhp + "/" + maxHp);    //テキストの更新
+                    });
+
+                    //HPがゼロになったらボタン表示
+                    if (myhp == 0){
+                        handler.post(()->{
+                            binding.battleEndButton.setText("リザルト画面へ");
+                            binding.battleEndButton.setVisibility(View.VISIBLE);
+                        });
+                        //ループ終了
+                        break;
+                    }
+
+
+                    try {
+                        Thread.sleep(50);   //50ミリ秒待つ
+                    }catch (InterruptedException e){
+
+                    }
+                }
+            }
+
+        }).start();
+
+
     }
 
+    protected void ShowEndButton(){
+        binding.battleEndButton.setVisibility(View.VISIBLE);
+    }
 
 }
