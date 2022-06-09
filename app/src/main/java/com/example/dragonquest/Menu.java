482ee45cbbb0d5a13f11c23cf5e7d69c4eab77d5
tyami05 <ryoumasato0118@gmail.com>
2022-06-09 13:22:47 +0900

    画面の編集1

--- app/src/main/java/com/example/dragonquest/Menu.java
+++ app/src/main/java/com/example/dragonquest/Menu.java
@@ -3,12 +3,18 @@ package com.example.dragonquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
 public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_menu);
+
+        Button returnButton = findViewById(R.id.button2);
+        // lambda式
+        returnButton.setOnClickListener(v -> finish());
+
     }
 }
