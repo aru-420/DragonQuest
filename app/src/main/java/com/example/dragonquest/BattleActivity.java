1208386e79805a062b29710799183085429d8ed8
kandaKONA <ivynk.kanda@outlook.jp>
2022-06-09 09:44:51 +0900

    初期設定

--- app/src/main/java/com/example/dragonquest/BattleActivity.java
+++ app/src/main/java/com/example/dragonquest/BattleActivity.java
@@ -3,12 +3,24 @@ package com.example.dragonquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import com.example.dragonquest.databinding.ActivityBattleBinding;
 
 public class BattleActivity extends AppCompatActivity {
+    private ActivityBattleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_battle);
+        binding = ActivityBattleBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.btn.setText("こんにちは");
+            }
+        });
     }
 }
