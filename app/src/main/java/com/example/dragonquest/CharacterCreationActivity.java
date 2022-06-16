d7913c5283fdcf5dce477ccd353ee5580ccdf10b
[aru-420] <[syota1202amr@ezweb.ne.jp]>
2022-06-16 10:32:41 +0900

    キャラ選択画面 育成画面　改善

--- app/src/main/java/com/example/dragonquest/CharacterCreationActivity.java
+++ app/src/main/java/com/example/dragonquest/CharacterCreationActivity.java
@@ -1,19 +1,29 @@
 package com.example.dragonquest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import com.example.dragonquest.databinding.ActivityBattleBinding;
+import com.example.dragonquest.databinding.ActivityCharacterCreationBinding;
 
 public class CharacterCreationActivity extends AppCompatActivity {
-    private ActivityBattleBinding binding;
+    private ActivityCharacterCreationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityBattleBinding.inflate(getLayoutInflater());
+        binding = ActivityCharacterCreationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
+        //開始ボタンの処理
+        binding.startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //NurtureActivityに画面遷移
+                Intent intent = new Intent(getApplication(), NurtureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dragonquest/NurtureActivity.java
+++ app/src/main/java/com/example/dragonquest/NurtureActivity.java
@@ -3,15 +3,15 @@ package com.example.dragonquest;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import com.example.dragonquest.databinding.ActivityBattleBinding;
+import com.example.dragonquest.databinding.ActivityNurtureBinding;
 
 public class NurtureActivity extends AppCompatActivity {
-    private ActivityBattleBinding binding;
+    private ActivityNurtureBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityBattleBinding.inflate(getLayoutInflater());
+        binding = ActivityNurtureBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
