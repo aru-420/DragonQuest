34b20d7a67e90e1d5809a124a93ba4da70a2c480
kandaKONA <ivynk.kanda@outlook.jp>
2022-06-16 10:16:23 +0900

    データベースとjsonの作成

--- app/src/main/java/com/example/dragonquest/BattleActivity.java
+++ app/src/main/java/com/example/dragonquest/BattleActivity.java
@@ -3,16 +3,25 @@ package com.example.dragonquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.TextView;
+import com.example.dragonquest.DBTables.SkillTable;
 
 import com.example.dragonquest.databinding.ActivityBattleBinding;
 
 public class BattleActivity extends AppCompatActivity {
     private ActivityBattleBinding binding;
+
+    //データベース接続用変数
+    private DatabaseHelper helper = null;
+
+    private String messagetext;
 
     private Handler handler = new Handler();//スレッド内でテキストをいじる際に使用
     private int myhp = 100; //キャラのHPを保存する変数
@@ -25,6 +34,9 @@ public class BattleActivity extends AppCompatActivity {
         binding = ActivityBattleBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        //データベース接続
+        helper = new DatabaseHelper(this);
 
         //初期非表示
         binding.battleMessage.setVisibility(View.INVISIBLE);    //バトルメッセージ非表示
@@ -68,16 +80,26 @@ public class BattleActivity extends AppCompatActivity {
                 //メッセージの表示
                 binding.battleMessage.setVisibility(View.VISIBLE);
                 binding.battleMessage.setText("スキル3が選択されました");
+                onSave();
             }
         });
 
         //右下ボタンクリック時の処理
         binding.skill4.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 //メッセージの表示
                 binding.battleMessage.setVisibility(View.VISIBLE);
-                binding.battleMessage.setText("スキル4が選択されましたqqqqqqqqqqqq");
+                messagetext = "スキル4が選択されました";
+                binding.battleMessage.setText(messagetext);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        messagetext = messagetext + "\n90ダメージ！";
+                        binding.battleMessage.setText(messagetext);// ここは1秒後に表示させたいです
+                    }
+                }, 500); // 1000ミリ秒（1秒）後
             }
         });
 
@@ -160,6 +182,76 @@ public class BattleActivity extends AppCompatActivity {
 
         }).start();
         //スレッドここまで
+    }
+
+
+    //データベース登録テスト
+    public void onSave(){
+        // 書き込みモードでデータベースをオープン
+        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+
+            // 入力されたタイトルとコンテンツをContentValuesに設定
+            // ContentValuesは、項目名と値をセットで保存できるオブジェクト
+            ContentValues cv = new ContentValues();
+
+            cv.put(SkillTable.COLUMN_NAME_SKILL_NAME, "スラッシュ2");
+            cv.put(SkillTable.COLUMN_NAME_SKILL_EFFECT, "100ダメージ");
+
+            // 現在テーブルに登録されているデータの_IDを取得
+            Cursor cursor = db.query(SkillTable.TABLE_NAME,  new String[] {SkillTable.COLUMN_NAME_SKILL_ID}, null, null,
+                    null, null, null, null);
+
+            // テーブルにデータが登録されていれば更新処理
+            if (cursor.moveToFirst()){
+
+                // 取得した_IDをparamsに設定
+                String[] params = {cursor.getString(0)};
+
+                // _IDのデータを更新
+                db.update(SkillTable.TABLE_NAME, cv, SkillTable.COLUMN_NAME_SKILL_ID + " = ?", params);
+
+            } else {
+
+                // データがなければ新規登録
+                db.insert(SkillTable.TABLE_NAME, null, cv);
+            }
+        }
+        onShow();
+    }
+
+    // データを表示する
+    protected void onShow() {
+
+        // データベースから取得する項目を設定
+        String[] cols = {SkillTable.COLUMN_NAME_SKILL_NAME, SkillTable.COLUMN_NAME_SKILL_EFFECT};
+
+        String sql = "select " + cols + " from " + SkillTable.TABLE_NAME + " WHERE skill_id = 1;";
+        // 読み込みモードでデータベースをオープン
+        try (SQLiteDatabase db = helper.getReadableDatabase()) {
+
+            // データを取得するSQLを実行
+            // 取得したデータがCursorオブジェクトに格納される
+            Cursor cursor = db.query(SkillTable.TABLE_NAME, cols, "skill_id = 1",
+                    null, null, null, null, null);
+
+            // moveToFirstで、カーソルを検索結果セットの先頭行に移動
+            // 検索結果が0件の場合、falseが返る
+            if (cursor.moveToFirst()){
+
+                // 表示用のテキスト・コンテンツに検索結果を設定
+                binding.battleMessage.setText(cursor.getString(0) + cursor.getString(1));
+
+
+            } else {
+                // 検索結果が0件の場合のメッセージを設定
+//                viewTitle.setText("データがありません");
+//                viewContents.setText("");
+//
+//                editTitle.setText("");
+//                editContents.setText("");
+            }
+        }
+
     }
 
 }
