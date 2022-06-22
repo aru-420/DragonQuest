d5d4894c7c2163eb5cca2726bfddf7779c6a258d
tyami05 <ryoumasato0118@gmail.com>
2022-06-22 13:41:17 +0900

    キャラ画面追加しました。

--- app/src/main/java/com/example/dragonquest/MyDialog.java
+++ app/src/main/java/com/example/dragonquest/MyDialog.java
@@ -6,14 +6,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
+
 public class MyDialog extends DialogFragment {
     @NonNull
     @Override
