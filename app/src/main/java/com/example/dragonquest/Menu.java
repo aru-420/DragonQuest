240f956ee073f1dd2aceb6b25c2c35cecda012ef
tyami05 <ryoumasato0118@gmail.com>
2022-06-22 13:07:06 +0900

    画面の編集1

--- app/src/main/java/com/example/dragonquest/Menu.java
+++ app/src/main/java/com/example/dragonquest/Menu.java
@@ -11,14 +11,30 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_menu);
 
         Button tap_continue = findViewById(R.id.continue1);
-        // lambda式
-        tap_continue.setOnClickListener(v -> finish());
+        //続けるボタンを押したとき
+        tap_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplication(), Chara.class);
+                startActivity(intent);
+            }
+        });
+
+        Button tap_status = findViewById(R.id.status);
+        //ステータスボタンを押したとき
+        tap_status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplication(), Chara.class);
+                startActivity(intent);
+            }
+        });
 
         Button tap_giveup = findViewById(R.id.giveup);
+        //あきらめるボタンを押したとき
         tap_giveup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
