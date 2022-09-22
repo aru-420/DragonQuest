21d55edca7cd2027f844b1aaadf3f2561a6a1966
kandaKONA <ivynk.kanda@outlook.jp>
2022-09-22 14:16:23 +0900

    ステータス画面作成

--- app/src/main/java/com/example/dragonquest/StatusActivity.java
+++ app/src/main/java/com/example/dragonquest/StatusActivity.java
@@ -38,45 +38,30 @@ public class StatusActivity extends AppCompatActivity {
 
         // ヘルパーを準備
         helper = new DatabaseHelper(this);
+        Actor get_save = (Actor) this.getApplication();
+        //仮読み込み
+        Actor actor = get_save.GetActivityActor();
 
-        // 読み込みモードでデータベースをオープン
-        try (SQLiteDatabase db = helper.getReadableDatabase()) {
+        hp = actor.getHp();
+        atk = actor.getAtk();
+        def = actor.getDef();
+        dex = actor.getDex();
+        skill1 = actor.getSkill1().getSkill_name();
+        skill2 = actor.getSkill2().getSkill_name();
+        skill3 = actor.getSkill3().getSkill_name();
+        skill4 = actor.getSkill4().getSkill_name();
 
-            // データベースから取得する項目を設定
-            String[] cols = {DBTables.CharacterTable.CHARA_SAVE_NAME, DBTables.CharacterTable.CHARA_SAVE_HP,
-                    DBTables.CharacterTable.CHARA_SAVE_ATK, DBTables.CharacterTable.CHARA_SAVE_DEF, DBTables.CharacterTable.CHARA_SAVE_DEX,
-                    DBTables.CharacterTable.CHARA_SAVE_SKILL1, DBTables.CharacterTable.CHARA_SAVE_SKILL2, DBTables.CharacterTable.CHARA_SAVE_SKILL3, DBTables.CharacterTable.CHARA_SAVE_SKILL4
-            };
-            //where文
-            String my_where = DBTables.CharacterTable.CHARA_SAVE_ID + " = 1";
+        //ステータス表示
+        binding.txtHP2.setText(Integer.toString(hp));
+        binding.txtATK2.setText(Integer.toString(atk));
+        binding.txtDEF2.setText(Integer.toString(def));
+        binding.txtDEX2.setText(Integer.toString(dex));
 
-            Cursor cursor;
-            cursor = db.query(DBTables.CharacterTable.TABLE_NAME, cols, my_where,
-                    null, null, null, null, null);
-
-            if (cursor.moveToFirst()) {
-                name = cursor.getString(0);
-                hp = Integer.parseInt(cursor.getString(1));
-                atk = Integer.parseInt(cursor.getString(2));
-                def = Integer.parseInt(cursor.getString(3));
-                dex = Integer.parseInt(cursor.getString(4));
-                skill1 = cursor.getString(5);
-                skill2 = cursor.getString(6);
-                skill3 = cursor.getString(7);
-                skill4 = cursor.getString(8);
-            }
-
-            //ステータス表示
-            binding.txtHP2.setText(Integer.toString(hp));
-            binding.txtATK2.setText(Integer.toString(atk));
-            binding.txtDEF2.setText(Integer.toString(def));
-            binding.txtDEX2.setText(Integer.toString(dex));
-
-            //スキル表示
-            binding.Skill1.setText(skill1);
-            binding.Skill2.setText(skill2);
-            binding.Skill3.setText(skill3);
-            binding.Skill4.setText(skill4);
+        //スキル表示
+        binding.Skill1.setText(skill1);
+        binding.Skill2.setText(skill2);
+        binding.Skill3.setText(skill3);
+        binding.Skill4.setText(skill4);
 
             //画像の変更
             if (name.equals(CharaNameStr[0])) {
@@ -88,7 +73,7 @@ public class StatusActivity extends AppCompatActivity {
                 ImageView imageView = findViewById(R.id.character_Image_nu);
                 imageView.setImageResource(R.drawable.magician);
             }
-        }
+
 
         binding.statusBack.setOnClickListener(new View.OnClickListener() {
             @Override
